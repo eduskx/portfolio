f9c9454469b61ecf0e226e3644c7e646b7bedf54
Eduard <eduard.lisovskij@gmail.com>
2024-09-05 12:04:55 +0200

    add CV

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,11 +1,15 @@
 import TechStack from "../TechStack";
 import styles from "./AboutMe.module.css";
+import Link from "next/link";
 
 export default function AboutMe({ text, techStack }) {
   return (
-    <section className={styles.container}>
+    <section>
       <h2 className={styles.title}>ÜBER MICH</h2>
       <p className={styles.text}>{text}</p>
+      <Link target="_blank" href="/public/Lebenslauf_Eduard_Lisovskij.pdf">
+        Lebenslauf herunterladen
+      </Link>
       <TechStack techStack={techStack} />
       <div className="section-divider"></div>
     </section>
